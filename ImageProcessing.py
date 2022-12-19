feddda790eefa7df5b37b587764f2509029db660
MohammedElshourbagi <96103950+MohammedElshourbagi@users.noreply.github.com>
2022-12-19 17:44:40 +0200

    FixedBugs in IP 2

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,4 +1,3 @@
-import os
 from numpy import asarray
 import cv2
 import matplotlib.pyplot as plt
@@ -8,4 +7,5 @@ def TranslateImageTo2DArray(path):
     original = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     (thresh, black_white) = cv2.threshold(original, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     half_sized = cv2.resize(black_white, (0, 0), fx=0.5, fy=0.5)
+
     return asarray(half_sized)
